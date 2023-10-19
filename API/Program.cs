9efba9a7197594e9cf99f2421025de10351d08d2
Karol <karolk1232.kk@gmail.com>
2023-10-19 20:08:25 +0200

    Added user authentication and authorization with JWT

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using API;
 using API.Data;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,18 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API;
+
+public class TokenService : ITokenService
+{
+
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration config)
+    {
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        var calims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+        };
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature); // siging the token
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(calims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor); 
+
+        return tokenHandler.WriteToken(token);
+
+    }
+}
