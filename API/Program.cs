b4c99d28f2be3f528d5d43982def2ca322dd3da3
Karol <karolk1232.kk@gmail.com>
2023-10-23 20:33:11 +0200

    Error handlings in API and Client applications

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
-using System.Text;
-using API;
-using API.Data;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +16,8 @@ builder.Services.AddIdentityService(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
